db510d44b936d3e064e381431e1fcc5e785beb8e
Valdir R. Lavandoski <valdir.lavandoski@bento.ifrs.edu.br>
2014-09-08 18:08:08 -0300

    Primeiro blog

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'asdasd3123e21QWE12!@W#@!@!ewqdqa');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', 'asd2134!#!#!DE!@EDDWQDQ@@@E#Wwsss');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
---
+++ app/Controller/PostsController.php
@@ -0,0 +1,14 @@
+<?php
+
+class PostsController extends AppController {
+	
+	public $helpers = array ('Html','Form');
+	public $name = 'Posts';
+
+	function index() {
+		$this->set('posts', $this->Post->find('all'));
+	}
+	
+}
+
+?>
