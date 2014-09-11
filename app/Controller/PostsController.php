973290b5ffa7da5f8bc8ac3ec117058b9b9b8964
Valdir R. Lavandoski <valdir.lavandoski@bento.ifrs.edu.br>
2014-09-11 18:22:16 -0300

    2014-09-11

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -4,9 +4,36 @@ class PostsController extends AppController {
 	
 	public $helpers = array ('Html','Form');
 	public $name = 'Posts';
+	public $components = array('Session');
 
 	function index() {
 		$this->set('posts', $this->Post->find('all'));
+	}
+	
+	public function view($id = null) {
+		$this->Post->id = $id;
+		$this->set('post', $this->Post->read());
+	}
+	
+	public function add() {
+		if ($this->request->is('post')) {
+			if ($this->Post->save($this->request->data)) {
+				$this->Session->setFlash('Your post has been saved.');
+				$this->redirect(array('action' => 'index'));
+			}
+		}
+	}
+	
+	function edit($id = null) {
+		$this->Post->id = $id;
+		if ($this->request->is('get')) {
+			$this->request->data = $this->Post->read();
+		} else {
+			if ($this->Post->save($this->request->data)) {
+				$this->Session->setFlash('Your post has been updated.');
+				$this->redirect(array('action' => 'index'	));
+			}
+		}
 	}
 	
 }
--- app/View/Posts/index.ctp
+++ app/View/Posts/index.ctp
@@ -1,5 +1,6 @@
 <!-- File: /app/View/Posts/index.ctp -->
 <h1>Posts do Blog</h1>
+	<?php echo $this->Html->link('Add Post', array('controller' => 'posts', 'action' => 'add'));?>
 	<table>
 		<tr>
 			<th>Id</th>
@@ -8,11 +9,11 @@
 		</tr>
 <!-- Aqui é onde nós percorremos nossa matriz $posts, imprimindo as informações dos posts -->
 
-<?php foreach ($posts as $post): ?>
+<?php foreach ($posts as $post){ ?>
 	<tr>
 		<td><?php echo $post['Post']['id']; ?></td>
 		<td><?php echo $this->Html->link($post['Post']['title'], array('controller' => 'posts', 'action' => 'view', $post['Post']['id'])); ?></td>
 		<td><?php echo $post['Post']['created']; ?></td>
 	</tr>
-<?php endforeach; ?>
+<?php } ?>
 </table>
